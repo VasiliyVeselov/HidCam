14746288813bafb33dac6ed27e4eb1b6771d6d22
VasiliyVeselov <veselov1503@mail.ru>
2023-03-21 11:54:45 +0300

    no

--- app/src/main/java/com/noname/hidcam/MainActivity.java
+++ app/src/main/java/com/noname/hidcam/MainActivity.java
@@ -2,43 +2,142 @@ package com.noname.hidcam;
 
 import android.Manifest;
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Size;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageAnalysis;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
 
     static int PERMISSION_ALL = 1;
     String[] PERMISSIONS = {Manifest.permission.CAMERA,Manifest.permission.RECORD_AUDIO,Manifest.permission.WRITE_EXTERNAL_STORAGE};
 
+    ListenableFuture<ProcessCameraProvider> cameraProviderFuture;
+
+    PreviewView previewView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        previewView = findViewById(R.id.preview_view);
+
+
 
         if (!hasPermissions(this, PERMISSIONS)) {
             ActivityCompat.requestPermissions(this, PERMISSIONS, PERMISSION_ALL);
         } else {
-            Intent intent = new Intent(this, VideoRecordingService.class);
-            startForegroundService(intent);
+            //Intent intent = new Intent(this, VideoRecordingService.class);
+            //startForegroundService(intent);
+            try {
+                initializeCamera();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
         }
+
+
 
         Button buttonStop = findViewById(R.id.buttonStop);
         buttonStop.setOnClickListener(view -> {
-            stopService(new Intent(this, VideoRecordingService.class));
+            //stopService(new Intent(this, VideoRecordingService.class));
+
 
 
         });
 
+
+
+
+
+
+
+
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+
+
+
+
+    private void initializeCamera() throws IOException {
+        cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+
+                Preview preview = new Preview.Builder()
+                        .build();
+
+                preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+
+                //ImageCapture imageCapture = new ImageCapture.Builder().build();
+
+                ImageAnalysis imageAnalysis = new ImageAnalysis.Builder()
+                        .setTargetResolution(new Size(1024, 768))
+                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                        .build();
+
+                CameraSelector cameraSelector = new CameraSelector.Builder()
+                        .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                        .build();
+
+
+
+
+
+                cameraProvider.bindToLifecycle(MainActivity.this, cameraSelector, preview, imageAnalysis);
+
+            } catch (ExecutionException | InterruptedException e) {
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+
+
+
+    }
+
+    private void stopRecording() {
+
+    }
+
+
 
     public boolean hasPermissions(Context context, String... permissions) {
         if (context != null && permissions != null) {
@@ -56,13 +155,29 @@ public class MainActivity extends AppCompatActivity {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == PERMISSION_ALL) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                Intent intent = new Intent(this, VideoRecordingService.class);
-                startForegroundService(intent);
+                //Intent intent = new Intent(this, VideoRecordingService.class);
+                //startForegroundService(intent);
+                try {
+                    initializeCamera();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+
             } else {
                 Toast.makeText(this, "NEED PERMISSIONS",
                         Toast.LENGTH_LONG).show();
             }
         }
+    }
+    private File createVideoFile(String position) throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault())
+                .format(new Date());
+
+        File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES).getAbsolutePath());
+
+        //File storageDir = getExternalFilesDir(Environment.DIRECTORY_MOVIES);
+
+        return File.createTempFile("VIDEO_" + position + "_" + timeStamp,".MP4", storageDir);
     }
 
 }
